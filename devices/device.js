6fce0d87e39df64f752dcf472948a05046d5b092
unknown <928805323@qq.com>
2022-04-18 17:02:20 +0800

    修改

--- devices/device.js
+++ devices/device.js
@@ -1,17 +1,5 @@
 module.exports = [
     // Buid-in device
-    'arduinoUno',
-    'arduinoNano_arduinoUno',
-    'arduinoMini_arduinoUno',
-    'arduinoLeonardo',
-    'arduinoMega2560',
-    'arduinoEsp32',
-    'arduinoEsp8266',
-    'microbit',
-    'microbitV2',
-    'makeyMakey',
-    // Third party
-    'ironKit_arduinoUno',
-    'QDPRobot_arduinoUnoUltra',
-    'QDPRobotC02_arduinoEsp32'
+    'robotmain',
+    'robotTE'
 ];
